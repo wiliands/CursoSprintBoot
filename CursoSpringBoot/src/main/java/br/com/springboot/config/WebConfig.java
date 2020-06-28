9a32b2c16c2a8a19fcab613b406e6997e79b4166
Wilian Domingues <wiliands@gmail.com>
2020-06-28 16:40:06 -0300

    Secao 16 - CORS

--- CursoSpringBoot/src/main/java/br/com/springboot/config/WebConfig.java
+++ CursoSpringBoot/src/main/java/br/com/springboot/config/WebConfig.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.springboot.converters.serialization.YamlJackson2HttpMessageConverter;
 import br.com.springboot.util.CustomMediaType;
+import br.com.springboot.util.HttpMethodsUtil;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -48,6 +50,11 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
+	}
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods(HttpMethodsUtil.getHttpMethods());
 	}
 
 }
