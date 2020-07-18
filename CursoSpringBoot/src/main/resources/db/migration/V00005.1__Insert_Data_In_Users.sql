92394786e50d5723501f71e016944fdf06abcdbc
Wilian Domingues <wiliands@gmail.com>
2020-07-18 18:17:24 -0300

    Secao 21 - Docker/Travis

--- CursoSpringBoot/src/main/resources/db/migration/V00005.1__Insert_Data_In_Users.sql
+++ CursoSpringBoot/src/main/resources/db/migration/V00005.1__Insert_Data_In_Users.sql
@@ -1,2 +1,2 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('wilian', 'Wilian Domingues', '$2a$16$9qr2tv0HmXbHBsx.TZFjfux742wCZM32a8Wi6iBqwIqaizlHPuxHS', b'1', b'1', b'1', b'1');
+	('leandro', 'Leandro', '$2a$16$9qr2tv0HmXbHBsx.TZFjfux742wCZM32a8Wi6iBqwIqaizlHPuxHS', b'1', b'1', b'1', b'1');
---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM mysql:8.0.19
+EXPOSE 3306
